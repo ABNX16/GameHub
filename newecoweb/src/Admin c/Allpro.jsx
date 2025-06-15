2f79f9635f929160d6834ec8e657dbad0bc16f6c
abu <aboobackerpnaushad@gmail.com>
2025-06-16 01:01:54 +0530

    final added11

--- newecoweb/src/Admin c/Allpro.jsx
+++ newecoweb/src/Admin c/Allpro.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Adminnav from './AdminNav';
+import.meta.env.VITE_BACKEND_URL
 
 function Allpro() {
   const [items, setItems] = useState([]);
